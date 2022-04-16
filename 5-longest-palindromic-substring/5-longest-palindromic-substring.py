5c0229a720d7f2ec3003961742f3810cc2962f02
Srishty Singh <48465143+srishty-singh2k@users.noreply.github.com>
2022-04-16 11:23:56 +0530

    Time: 3548 ms (20.97%), Space: 211.6 MB (5.72%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,21 +1,9 @@
-class Solution:
-    def isPalindrome(self, each:str) -> bool:
-        print("To check: ",each)
-        if len(each)%2==0:
-            if  ("".join(reversed(each[:len(each)//2]))) == each[len(each)//2:]:
-                return True
-        else:
-            if ("".join(reversed(each[:len(each)//2+1]))) == each[len(each)//2:]:
-                return True
-        return False
-
-                    
+class Solution:      
     def longestPalindrome(self, s: str) -> str:
         pali=[]
         even=[]
         for each in s:
             pali.append(each)
-        #print(pali)
         for i in range(len(s)-1):
             offset=0
             if s[i] == s[i+1]:
@@ -28,16 +16,12 @@ class Solution:
                     offset+=1
             offset=0
             while(i-offset>-1 and i+offset<len(s) and s[i-offset]==s[i+offset]):
-                #print("odd", i, offset, s[i-offset]==s[i+offset])
                 if i+offset+1 ==len(s):
                     pali.append(s[i-offset:])
                 else:
                     pali.append(s[i-offset: i+offset+1])
                 offset+=1
-                
-        #print("pali",pali)
-        #print("even",even)
-        
+
         if pali==[]:
             return ""
         return max(pali, key=len)
