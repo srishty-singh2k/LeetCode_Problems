463be150691a51f2d9590ed16083575b36821ecb
Srishty Singh <48465143+srishty-singh2k@users.noreply.github.com>
2022-09-06 12:22:24 +0530

    Time: 40 ms (91.41%), Space: 13.9 MB (31.83%) - LeetHub

--- 61-rotate-list/61-rotate-list.py
+++ 61-rotate-list/61-rotate-list.py
@@ -6,22 +6,19 @@
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         if k == 0 or not head: return head
-        fast = slow = head
-        i = 1
-        while(i<=k and fast):
-            fast = fast.next
-            i+=1
-        if fast:
-            while(fast.next):
-                fast = fast.next
-                slow = slow.next
-            temp = slow.next
-            slow.next = None
-            fast.next = head
-            head = temp
+        current = head
+        len_ = 1
+        while(current.next):
+            current = current.next
+            len_ += 1
+        if k % len_ == 0:
             return head
-        if k % (i-1) == 0:
-            return head
-        return self.rotateRight(head, k%(i-1))
-            
+        current.next = head
+        k = len_ - (k%len_)
+        while(k):
+            current = current.next
+            k-=1
+        head = current.next
+        current.next = None
+        return head
             
